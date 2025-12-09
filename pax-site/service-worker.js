ab207f20600c50cb856f482000ebc0e9fc60191c
Rayzeal24 <yukarinegima1@gmail.com>
2025-12-09 20:02:00 +0100

    Delete pax-site directory

--- pax-site/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("Service worker installé (Mini Pax)");
-  // Plus tard, tu pourras ajouter ici la mise en cache des fichiers.
-});
-
-self.addEventListener("fetch", (event) => {
-  // Pour l'instant, on laisse passer toutes les requêtes normalement.
-});
