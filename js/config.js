511f622d25569b231036f7787fb55914bfd41fdc
Rayzeal24 <yukarinegima1@gmail.com>
2025-12-12 20:41:04 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+window.POLITARIA = window.POLITARIA || {};
+
+POLITARIA.CONFIG = {
+  ROUTES: {
+    HOME: "/index.html",
+    GAMES: "/games.html"
+  },
+  KEYS: {
+    MODE: "politariaMode",
+    EMAIL: "politariaEmail",
+    COINS: "politariaCoins",
+    SYSTEMS: "politariaSystems_v1"
+  }
+};
