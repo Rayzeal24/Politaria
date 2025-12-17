210f175c2c47db649b0cd2e5c1f7071dd3872b52
Rayzeal24 <yukarinegima1@gmail.com>
2025-12-17 13:57:27 +0100

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+// /js/supabase.js
+const SUPABASE_URL = "https://pgzmquaaopevyqveksir.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Bemb9WWHzBAXQt5yC-3qDw_tLxNu_h5";
+
+window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
