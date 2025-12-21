b503f65cb25bd7bde0d0f73ec1cf730c3f379cfc
Rayzeal24 <yukarinegima1@gmail.com>
2025-12-21 21:58:13 +0100

    Update input.js

--- js/engine/input.js
+++ js/engine/input.js
@@ -1,1 +1,70 @@
+import { clamp } from "./math.js";
+
+export class Input {
+  constructor(canvas) {
+    this.canvas = canvas;
+
+    this.mx = 0;
+    this.my = 0;
+    this._down = false;
+    this._clicked = false;
+
+    this._rect = canvas.getBoundingClientRect();
+    this._needsRect = true;
+
+    this._onMove = (e) => this._handleMove(e);
+    this._onDown = (e) => this._handleDown(e);
+    this._onUp = () => this._handleUp();
+    this._onResize = () => { this._needsRect = true; };
+
+    canvas.addEventListener("mousemove", this._onMove);
+    canvas.addEventListener("mousedown", this._onDown);
+    window.addEventListener("mouseup", this._onUp);
+    window.addEventListener("resize", this._onResize);
+  }
+
+  destroy() {
+    this.canvas.removeEventListener("mousemove", this._onMove);
+    this.canvas.removeEventListener("mousedown", this._onDown);
+    window.removeEventListener("mouseup", this._onUp);
+    window.removeEventListener("resize", this._onResize);
+  }
+
+  beginFrame() {
+    this._clicked = false;
+    if (this._needsRect) {
+      this._rect = this.canvas.getBoundingClientRect();
+      this._needsRect = false;
+    }
+  }
+
+  endFrame() {}
+
+  consumeClick() {
+    const c = this._clicked;
+    this._clicked = false;
+    return c;
+  }
+
+  get down() { return this._down; }
+
+  _handleMove(e) {
+    const r = this._rect;
+    const x = (e.clientX - r.left) / r.width * this.canvas.width;
+    const y = (e.clientY - r.top) / r.height * this.canvas.height;
+    this.mx = clamp(x, 0, this.canvas.width);
+    this.my = clamp(y, 0, this.canvas.height);
+  }
+
+  _handleDown(e) {
+    if (e.button !== 0) return;
+    this._down = true;
+    this._clicked = true;
+    this._handleMove(e);
+  }
+
+  _handleUp() {
+    this._down = false;
+  }
+}
 
