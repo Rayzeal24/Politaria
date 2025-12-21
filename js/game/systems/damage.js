fe17e84e027a37ef676083a6fec9a7ec943e4299
Rayzeal24 <yukarinegima1@gmail.com>
2025-12-21 22:01:35 +0100

    Update damage.js

--- js/game/systems/damage.js
+++ js/game/systems/damage.js
@@ -1,1 +1,13 @@
+export function applyDamage(enemy, amount, dmgType) {
+  // dmgType: "phys" | "magic"
+  let mult = 1;
+  if (dmgType === "phys") {
+    mult = 1 - enemy.armor;      // armor: 0..1
+  } else if (dmgType === "magic") {
+    mult = 1 - enemy.mResist;    // mResist: 0..1
+  }
+  const dealt = amount * (mult < 0 ? 0 : mult);
+  enemy.hp -= dealt;
+  return dealt;
+}
 
