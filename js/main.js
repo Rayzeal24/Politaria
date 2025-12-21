d8ecf42e05ffe24519035bc113702240b9899ac8
Rayzeal24 <yukarinegima1@gmail.com>
2025-12-21 21:57:17 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+import { Engine } from "./engine/engine.js";
+import { Input } from "./engine/input.js";
+import { Game } from "./game/game.js";
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d", { alpha: true });
+
+const input = new Input(canvas);
+const game = new Game(canvas, ctx, input);
+
+const engine = new Engine({
+  update: (dt) => game.update(dt),
+  render: () => game.render(),
+});
+
+engine.start();
+
